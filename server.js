efd8de312c7938bca71ca0846cdfd40e975a5e16
Eysseric Lawrence <leysseric82400@gmail.com>
2025-03-04 22:40:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 let db;
 
@@ -118,7 +118,7 @@ async function startServer() {
     });
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../frontend/index.html'));
+      res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
     const PORT = process.env.PORT || 3000;
